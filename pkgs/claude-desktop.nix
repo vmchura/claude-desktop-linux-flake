83fa37319f07bca47278f344b45af1d46ce2feca
Keith <k@3d3.ca>
2025-01-11 14:35:44 -0500

    Make a clarification comment

--- pkgs/claude-desktop.nix
+++ pkgs/claude-desktop.nix
@@ -14,6 +14,7 @@
   pname = "claude-desktop";
   version = "0.7.8";
   srcExe = fetchurl {
+    # NOTE: `?v=0.7.8` doesn't actually request a specific version. It's only being used here as a cache buster.
     url = "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe?v=0.7.8";
     hash = "sha256-SOO1FkAfcOP50Z4YPyrrpSIi322gQdy9vk0CKdYjMwA=";
   };
