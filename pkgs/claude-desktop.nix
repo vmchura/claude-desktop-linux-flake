54bb0056d5c675a7e6922458e48dba5a500707f3
Drake Bott <me@drake.dev>
2025-06-02 19:57:52 -0500

    chore: Update to 0.9.4

--- pkgs/claude-desktop.nix
+++ pkgs/claude-desktop.nix
@@ -13,11 +13,11 @@
   perl
 }: let
   pname = "claude-desktop";
-  version = "0.9.3";
+  version = "0.9.4";
   srcExe = fetchurl {
     # NOTE: `?v=0.9.0` doesn't actually request a specific version. It's only being used here as a cache buster.
-    url = "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe?v=0.9.3";
-    hash = "sha256-uzRiNsvOUEVg+qZVJiRNGGUHpqGdGt7it/DFi7DHqCw=";
+    url = "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe?v=0.9.4";
+    hash = "sha256-OofXsRNVBueiewCCfYOwGqlsZECYtWWRTg4Wu+hGAnE=";
   };
 in
   stdenvNoCC.mkDerivation rec {
