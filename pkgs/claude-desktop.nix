f7163e011750acf53a26e1153f05e5905054857c
Linus Jansson <linus.jansson@mediatool.com>
2025-06-17 15:52:16 +0200

    fix: update version with hash

--- pkgs/claude-desktop.nix
+++ pkgs/claude-desktop.nix
@@ -13,10 +13,10 @@
   perl
 }: let
   pname = "claude-desktop";
-  version = "0.10.14";
+  version = "0.10.38";
   srcExe = fetchurl {
     # NOTE: `?v=0.10.0` doesn't actually request a specific version. It's only being used here as a cache buster.
-    url = "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe?v=0.10.14";
+    url = "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest-win-x64/Claude-Setup-x64.exe?v=0.10.38";
     hash = "sha256-RRXa1PCHBOMdSKcCFr4rmqzWBiA/ezSuz+mWhc0zbkE=";
   };
 in
